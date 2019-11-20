07538ba7a680b429416506e06b094b705958bb95
sunilsarode <sarodesunil7@gmail.com>
2019-11-20 23:59:01 +0530

    comment pass testing

---
+++ comments/test.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+  //this my first comment \
+  new line started
+
+  //this is second \
+  dgfgfg
+/**  
+ * 
+ * //--> nestetd comment 
+ *
+ * \\\\
+ * 
+ * */
+int nest = /*/*/0*/**/1;
+
+//seocond test \
+sunil let it go man , do not brood over it...
+
+/**
+ *  this is main funtion
+ *  //ok let see  
+ */
+
+
+int main(){
+
+    cout<<nest<<endl;    
+    return 0;
+}
+
+
+//end of comments \
+
+/* fdfdf s
+
+dfadfjhg
+dfdsghs
+
+*/ 
+
+//this is last comment
